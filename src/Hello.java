f0719c60fee2b17f50f37b7c10ce432807d4568d
Jake <Jake@LAPTOP-E7A9ESTA.unl.edu>
2021-08-25 15:13:50 -0700

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * Author: Jake Nichols (jnichols12@huskers.unl.edu)
+ * Date: 8/25/2021
+ * 
+ * A simple program that prints my name
+ * 
+ * @author Jnichols12
+ *
+ */
+
+public class Hello {
+
+	public static void main(String args[]) {
+		
+		System.out.println("Jake Nichols");
+		
+	}
+	
+}
